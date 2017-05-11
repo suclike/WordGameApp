ab8b83b12468145b72366fa2e4385ead7efbdcb1
Pavel Zagalsky <pavel.zagalsky@zalando.de>
2017-05-12 00:09:59 +0200

    ---- ### Commit message ### ---- base activity added split

---
+++ app/src/main/java/de/sample/wordgame/wordgameapplication/ui/BaseActivity.java
@@ -0,0 +1,44 @@
+package de.sample.wordgame.wordgameapplication.ui;
+
+import android.os.Bundle;
+
+import android.support.annotation.LayoutRes;
+
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+import de.sample.wordgame.wordgameapplication.WordGameApplication;
+import de.sample.wordgame.wordgameapplication.di.ActivityComponent;
+import de.sample.wordgame.wordgameapplication.di.ApplicationComponent;
+import de.sample.wordgame.wordgameapplication.di.module.ActivityModule;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private ActivityComponent activityComponent;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+
+        // handling injection
+        activityComponent = createActivityComponent();
+        handleInjection(activityComponent);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(layoutToInflate());
+
+        ButterKnife.bind(this);
+    }
+
+    private ActivityComponent createActivityComponent() {
+        ApplicationComponent applicationComponent = ((WordGameApplication) getApplication()).applicationComponent;
+        ActivityModule activityInstanceModule = new ActivityModule(this);
+        return applicationComponent.plus(activityInstanceModule);
+    }
+
+    protected abstract void handleInjection(final ActivityComponent component);
+
+    @LayoutRes
+    protected abstract int layoutToInflate();
+}
