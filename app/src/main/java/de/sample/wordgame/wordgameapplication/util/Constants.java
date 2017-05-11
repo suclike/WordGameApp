f81ccf26931efed2b2889ba3376e7a34553751f4
Pavel Zagalsky <pavel.zagalsky@zalando.de>
2017-05-12 00:06:05 +0200

    ---- ### Commit message ### ---- constants added

---
+++ app/src/main/java/de/sample/wordgame/wordgameapplication/util/Constants.java
@@ -0,0 +1,7 @@
+package de.sample.wordgame.wordgameapplication.util;
+
+public class Constants {
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String WORDS_JSON_FILE_PATH = "words.json";
+}
