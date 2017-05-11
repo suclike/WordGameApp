3ce887d795f0753aed451b92ba703239dbdf186c
Pavel Zagalsky <pavel.zagalsky@zalando.de>
2017-05-12 00:08:07 +0200

    ---- ### Commit message ### ---- recycleview types added

---
+++ app/src/main/java/de/sample/wordgame/wordgameapplication/ui/adapter/ListViewTypes.java
@@ -0,0 +1,10 @@
+package de.sample.wordgame.wordgameapplication.ui.adapter;
+
+import static de.sample.wordgame.wordgameapplication.ui.adapter.ListViewTypes.VIEW_HOLDER_ITEM;
+
+import android.support.annotation.IntDef;
+
+@IntDef({ VIEW_HOLDER_ITEM })
+@interface ListViewTypes {
+    int VIEW_HOLDER_ITEM = 1;
+}
